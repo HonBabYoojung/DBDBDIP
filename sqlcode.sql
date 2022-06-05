35be31dd54098844fc5dfb79c0ced27df7252d37
singjun <shinsj4653@gmail.com>
2022-06-05 14:40:31 +0900

    mysql 코드 업데이트

--- sqlcode.sql
+++ sqlcode.sql
@@ -3,9 +3,8 @@ use dbproject_navermovie;
 
 # Movies 테이블
 create table Movies(
-	movie_id int primary key auto_increment,
-    title varchar(50) not null,
-    movie_code int not null,
+	movie_code integer not null primary key,
+    title varchar(200) not null,
     summary varchar(500),
     audience_rate float,
     audience_count varchar(10),
@@ -21,14 +20,15 @@ create table Movies(
 alter table Movies modify title mediumtext;
 alter table Movies modify summary mediumtext;
 
+
 # text가 너무 클시에 위의 alter mediumtext 필요!
 
 # Scope 테이블
 create table Scope(
 	scope_id int primary key auto_increment,
-	movie_id int not null,
+	movie_code int not null,
     scopeName varchar(100),
-    foreign key (movie_id) references Movies(movie_id)
+    foreign key (movie_code) references Movies(movie_code)
 );
 
 alter table Scope modify scopeName mediumtext;
@@ -45,3 +45,16 @@ create table Director(
 alter table Director modify directorName mediumtext;
 
 # Cast 테이블
+create table MovieCast (
+	cast_id int primary key auto_increment,
+    movie_code int,
+    castName varchar(100),
+    mainorsub varchar(10),
+    roleName varchar(100),
+    castImg varchar(300),
+    foreign key (movie_code) references Movies(movie_code)
+);
+
+alter table MovieCast modify castName mediumtext;
+alter table MovieCast modify roleName mediumtext;
+alter table MovieCast modify castImg mediumtext;
